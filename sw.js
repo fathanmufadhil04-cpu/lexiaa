6c496389345e85a1c1c4404bd3a7ef0df233893e
fathanmufadhil04-cpu <fathanmufadhil04@gmail.com>
2026-09-25 11:48:02 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lexiaa-new-v23-admin-panel';
+const C='lexiaa-new-v25-deluxe-effects';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
