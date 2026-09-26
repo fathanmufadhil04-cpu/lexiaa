aafad25bcf1a72f87fb8c190dd6fb7e22dfa53d2
fathanmufadhil04-cpu <fathanmufadhil04@gmail.com>
2026-09-26 09:18:33 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='lexiaa-new-v31-profile-aura-exchange-fix6-voucher-fix2';
+const C='lexiaa-new-v32-premium-aura-v1';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
